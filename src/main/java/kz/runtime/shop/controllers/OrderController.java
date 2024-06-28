cdfc1dd462ea519701c208c916b9b6994403e46f
Kasht <kubtimur1@mail.com>
2024-06-29 00:09:27 +0500

    fixing an error in the GetOrders method controller and styling the orders page

--- src/main/java/kz/runtime/shop/controllers/OrderController.java
+++ src/main/java/kz/runtime/shop/controllers/OrderController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class OrderController {
@@ -24,10 +27,14 @@ public class OrderController {
         List<Order> orders = orderService.getAllOrders();
         model.addAttribute("orders", orders);
 
+        Map<Long, List<OrderProduct>> orderProductMap = new HashMap<>();
+
         for (Order order : orders) {
             List<OrderProduct> orderProductsList = orderProductRepository.findOrderProductByOrder(order);
-            model.addAttribute("order_products", orderProductsList);
+            orderProductMap.put(order.getId(), orderProductsList);
         }
+
+        model.addAttribute("order_product_map", orderProductMap);
 
         return "orders";
     }
